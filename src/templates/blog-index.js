44876360a6d2848ade5191f7c5525cfadc1c67a4
NouranBakry <nouranaymanbakry@gmail.com>
2020-08-26 05:44:35 +0200

    fixes for gatsby

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -1,69 +1,122 @@
-import { Link, graphql } from 'gatsby';
-import { formatPostDate, formatReadingTime } from '../utils/helpers';
+import {
+    Link,
+    graphql
+} from 'gatsby';
+import {
+    formatPostDate,
+    formatReadingTime
+} from '../utils/helpers';
 
 import Bio from '../components/Bio';
 import Footer from '../components/Footer';
 import Layout from '../components/Layout';
+import Panel from '../components/Panel';
 import React from 'react';
 import SEO from '../components/SEO';
 import get from 'lodash/get';
-import { rhythm } from '../utils/typography';
+import {
+    rhythm
+} from '../utils/typography';
 
 class BlogIndexTemplate extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const langKey = this.props.pageContext.langKey;
+    render() {
+        const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+        const langKey = this.props.pageContext.langKey;
 
-    const posts = get(this, 'props.data.allMarkdownRemark.edges');
+        const posts = get(this, 'props.data.allMarkdownRemark.edges');
 
-    return (
-      <Layout location={this.props.location}>
-        <SEO />
-        <aside>
-          <Bio />
-        </aside>
-        <main>
-          {posts.map(({ node }) => {
-            const title = get(node, 'frontmatter.title') || node.fields.slug;
-            return (
-              <article key={node.fields.slug}>
-                <header>
-                  <h3
-                    style={{
-                      fontFamily: 'Roboto Mono, monospace',
-                      fontSize: rhythm(1),
-                      marginBottom: rhythm(1 / 4),
-                    }}
-                  >
-                    <Link
-                      style={{ boxShadow: 'none' }}
-                      to={node.fields.slug}
-                      rel="bookmark"
-                    >
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>
-                    {formatPostDate(node.frontmatter.date, langKey)}
-                    {` • ${formatReadingTime(node.timeToRead)}`}
-                  </small>
-                </header>
-                <p
-                  dangerouslySetInnerHTML={{ __html: node.frontmatter.spoiler }}
-                />
-              </article>
-            );
-          })}
-        </main>
-        <Footer />
-      </Layout>
-    );
-  }
+        return ( <
+            Layout location = {
+                this.props.location
+            }
+            title = {
+                siteTitle
+            } >
+            <
+            SEO / >
+            <
+            aside >
+            <
+            Bio / >
+            <
+            /aside> <
+            main > {
+                langKey !== 'en' && langKey !== 'ru' && ( <
+                    Panel >
+                    These articles have been {
+                        ' '
+                    } <
+                    a href = "https://github.com/gaearon/overreacted.io#contributing-translations"
+                    target = "_blank"
+                    rel = "noopener noreferrer" >
+                    translated by the community <
+                    /a>
+                    . <
+                    /Panel>
+                )
+            }
+
+            {
+                posts.map(({
+                    node
+                }) => {
+                    const title = get(node, 'frontmatter.title') || node.fields.slug;
+                    return ( <
+                        article key = {
+                            node.fields.slug
+                        } >
+                        <
+                        header >
+                        <
+                        h3 style = {
+                            {
+                                fontFamily: 'Montserrat, sans-serif',
+                                fontSize: rhythm(1),
+                                marginBottom: rhythm(1 / 4),
+                            }
+                        } >
+                        <
+                        Link style = {
+                            {
+                                boxShadow: 'none'
+                            }
+                        }
+                        to = {
+                            node.fields.slug
+                        }
+                        rel = "bookmark" > {
+                            title
+                        } <
+                        /Link> < /
+                        h3 > <
+                        small > {
+                            formatPostDate(node.frontmatter.date, langKey)
+                        } {
+                            ` • ${formatReadingTime(node.timeToRead)}`
+                        } <
+                        /small> < /
+                        header > <
+                        p dangerouslySetInnerHTML = {
+                            {
+                                __html: node.frontmatter.spoiler
+                            }
+                        }
+                        /> < /
+                        article >
+                    );
+                })
+            } <
+            /main> <
+            Footer / >
+            <
+            /Layout>
+        );
+    }
 }
 
 export default BlogIndexTemplate;
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query($langKey: String!) {
     site {
       siteMetadata {
